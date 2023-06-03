e39c58e2e56c72acd32e39b55389afa9b82d9037
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-06-03 10:23:01 -0600

    Development (#9)
    * Cleaning up the test file a bit
    
    * Added some logic so that searching is more robust; cleaned up a test as well.
    
    * Adding in fixes based on questions posed.
    
    * Messing around with Bard using Python; FastAPI exploration.
    
    * Learning more about FastAPI so I had to fix some of my Postman tests to work wit this code now.
    
    * Changed tests to point to port 8000 for FastAPI instead of 5000 for Flask.

--- src/tests/Local_tests.py
+++ src/tests/Local_tests.py
@@ -3,24 +3,9 @@ import json
 import random
 
 
-# Validate GET request for a single user at /users endpoint
-def test_1_validate_object1_contents():
-    response = requests.get("http://127.0.0.1:5000/users")
-    response_body = response.json()
-    print(response_body)
-    user = response_body[0]  # get the first user in the list
-    assert response.status_code == 200
-    assert response.headers["Content-Type"] == "application/json"
-    assert user["userId"] == 1234
-    assert user["name"] == "John Smithson"
-    assert user["jobTitle"] == "Software Test Engineer I"
-    assert user["city"] == "Seattle"
-    print(user)
-
-
-# Validate GET request for all users at /users endpoint
-def test_2_validate_objects_returned():
-    response = requests.get("http://127.0.0.1:5000/users")
+# Validate GET request for all users using /users endpoint
+def test_1_validate_objects_returned():
+    response = requests.get("http://127.0.0.1:8000/users")
     response_body = response.json()
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/json"
@@ -28,8 +13,9 @@ def test_2_validate_objects_returned():
     print(response_body)
 
 
-def test_3_validate_object_creation():
-    url = "http://127.0.0.1:5000/users"
+# Validate POST request for new user creation using /users endpoint
+def test_2_validate_object_creation():
+    url = "http://127.0.0.1:8000/users"
     newObject = {
         "userId": random.randint(1000, 9999),
         "name": "John Tester",
@@ -48,8 +34,9 @@ def test_3_validate_object_creation():
     print(response_body)
 
 
-def test_4_validate_object_updates():
-    url = "http://127.0.0.1:5000/users"
+# Validate PATCH request for updating a user record using /users endpoint
+def test_3_validate_object_updates():
+    url = "http://127.0.0.1:8000/users"
     newUser = {
         "userId": random.randint(1000, 9999),  # Creates a new user with the following attributes
         "name": "John Tester",
@@ -63,11 +50,11 @@ def test_4_validate_object_updates():
     updatedUser = {
         "name": "John Tester UPDATED",  # Updates the user with the following attributes
         "city": "Seattle",
-        "userId": new_user_id           # userId must be included in the JSON payload which is why we captured it above
+        "userId": new_user_id  # userId must be included in the JSON payload which is why we captured it above
     }
     patchObject = requests.patch(url + f'/{new_user_id}', json=updatedUser)  # PATCH operation for the updated user info
     updated_response_body = patchObject.json()
     assert updated_response_body["userId"] == new_user_id
-    assert updated_response_body["name"] == "John Tester UPDATED"   # Validate that the name was successfully updated
-    assert updated_response_body["city"] == "Seattle"               # Validate that the city was successfully updated
+    assert updated_response_body["name"] == "John Tester UPDATED"  # Validate that the name was successfully updated
+    assert updated_response_body["city"] == "Seattle"  # Validate that the city was successfully updated
     print(updated_response_body)
