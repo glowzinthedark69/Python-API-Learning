12e7393d9ac98d91e4bb50300f1c85bbda60a5d5
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-06-14 08:33:13 -0600

    Adding in a Locations endpoint and cleaning up the app files (#14)
    * Adding in a Locations endpoint and cleaning up the app files and endpoint files in a more common way.
    
    * Adding a PATCH endpoint and adding tests for the entire Locations endpoint. More work to come.
    
    * Adding test for PUT/Locations endpoint.
    
    * Adding test for PUT/Locations endpoint. Made some fixes

---
+++ src/locations.py
@@ -0,0 +1,62 @@
+from typing import List, Optional
+from fastapi import HTTPException, Body, Path
+from pydantic import BaseModel
+from app import app
+
+locations = []
+
+
+class Location(BaseModel):
+    id: Optional[int] = None
+    city: Optional[str] = None
+    country: Optional[str] = None
+
+
+@app.post("/locations", response_model=Location, status_code=201)
+def create_location(location: Location = Body(...)):
+    locations.append(location)
+    return location
+
+
+@app.get("/locations", response_model=List[Location])
+def get_locations():
+    return locations
+
+
+@app.get("/locations/{location_id}", response_model=Location)
+def get_location(location_id: int = Path(..., ge=0)):
+    for location in locations:
+        if location.id == location_id:
+            return location
+    raise HTTPException(status_code=404, detail="Location not found")
+
+
+@app.put("/locations/{location_id}", response_model=Location)
+def update_location(location_id: int, location: Location = Body(...)):
+    for l in locations:
+        if l.id == location_id:
+            l.city = location.city
+            l.country = location.country
+            return l
+    raise HTTPException(status_code=404, detail="Location not found")
+
+
+@app.patch("/locations/{location_id}", response_model=Location)
+def patch_location(location_id: int, location: Location = Body(...)):
+    for l in locations:
+        if l.id == location_id:
+            if location.city is not None:
+                l.city = location.city
+            if location.country is not None:
+                l.country = location.country
+            return l
+    raise HTTPException(status_code=404, detail="Location not found")
+
+
+@app.delete("/locations/{location_id}")
+def delete_location(location_id: int):
+    for location in locations:
+        if location.id == location_id:
+            locations.remove(location)
+            return {"message": "Location has been successfully deleted."}
+    raise HTTPException(status_code=404, detail="Location not found")
