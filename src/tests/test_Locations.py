45b0577a28657058b0bcbe6788267b2fc5db2fd8
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-06-14 08:28:19 -0600

    Adding test for PUT/Locations endpoint.

--- src/tests/test_Locations.py
+++ src/tests/test_Locations.py
@@ -17,7 +17,7 @@ def test_1_validate_objects_returned():
 def test_2_validate_object_creation():
     url = "http://127.0.0.1:8000/locations"
     newObject = {
-        "id": random.randint(10000, 99999),     # Creates a new user with the following attributes
+        "id": random.randint(10000, 99999),  # Creates a new user with the following attributes
         "city": "Denver AUTOMATED",
         "country": "United States AUTOMATED",
     }
@@ -50,7 +50,31 @@ def test_3_validate_object_updates():
     }
     patchObject = requests.patch(url + f'/{location_id}', json=updatedUser)  # PATCH operation for updated user info
     updated_response_body = patchObject.json()
-    assert updated_response_body["id"] == location_id   # Validate id matches as expected
+    assert updated_response_body["id"] == location_id  # Validate id matches as expected
     assert updated_response_body["country"] == "United States AUTOMATED (PATCH)"  # Validate country was updated
     assert updated_response_body["city"] == "Denver AUTOMATED (PATCH)"  # Validate city was successfully updated
+    print(updated_response_body)
+
+
+# Validate PATCH request for updating a user record using /users endpoint
+def test_4_validate_object_updates():
+    url = "http://127.0.0.1:8000/locations"
+    newUser = {
+        "id": random.randint(10000, 99999),  # Creates a user with the following attributes
+        "city": "Denver PUT",
+        "country": "United States PUT",
+    }
+    postObject = requests.post(url, json=newUser)  # POST operation for updating the new user
+    response_body = postObject.json()  # Converts the response into JSON
+    location_id = response_body["id"]  # Assigns the userId to the new_user_id variable for later use
+    updatedUser = {
+        "city": "Denver AUTOMATED (PUT)",  # Updates the user with the following attributes
+        "country": "United States AUTOMATED (PUT)",
+        "id": location_id  # userId must be included in the JSON payload which is why we captured it above
+    }
+    patchObject = requests.put(url + f'/{location_id}', json=updatedUser)  # PATCH operation for updated user info
+    updated_response_body = patchObject.json()
+    assert updated_response_body["id"] == location_id  # Validate id matches as expected
+    assert updated_response_body["country"] == "United States AUTOMATED (PUT)"  # Validate country was updated
+    assert updated_response_body["city"] == "Denver AUTOMATED (PUT)"  # Validate city was successfully updated
     print(updated_response_body)
