256b4405b9d6eba82d4bcc34c3598aa4da849315
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-08-03 11:05:07 -0600

    Adding DELETE tests for /users and /locations

--- tests/test_Locations.py
+++ tests/test_Locations.py
@@ -37,7 +37,7 @@ def test_2_validate_object_creation():
 # Validate PATCH request for updating a location record using /location endpoint
 def test_3_validate_object_updates_patch():
     url = "http://127.0.0.1:8000/locations"
-    new_user = {
+    new_location = {
         "id": random.randint(
             10000, 99999
         ),  # Creates a location with the following attributes
@@ -45,28 +45,28 @@ def test_3_validate_object_updates_patch():
         "country": "United States PATCH",
     }
     post_object = requests.post(
-        url, json=new_user
+        url, json=new_location
     )  # POST operation for updating the new location
     response_body = post_object.json()  # Converts the response into JSON
     location_id = response_body[
         "id"
     ]  # Assigns the id to the location_id variable for later use
-    updated_user = {
+    updated_location = {
         "city": "Denver AUTOMATED (PATCH)",  # Updates the location with the following attributes
         "country": "United States AUTOMATED (PATCH)",
         "id": location_id,  # id must be included in the JSON payload which is why we captured it above
     }
     assert response_body["id"] == location_id
     patch_object = requests.patch(
-        url + f"/{location_id}", json=updated_user
+        url + f"/{location_id}", json=updated_location
     )  # PATCH operation for updated location info
     updated_response_body = patch_object.json()
     assert updated_response_body["id"] == location_id  # Validate id matches as expected
     assert (
-        updated_response_body["country"] == "United States AUTOMATED (PATCH)"
+            updated_response_body["country"] == "United States AUTOMATED (PATCH)"
     )  # Validate country was updated
     assert (
-        updated_response_body["city"] == "Denver AUTOMATED (PATCH)"
+            updated_response_body["city"] == "Denver AUTOMATED (PATCH)"
     )  # Validate city was successfully updated
     print(updated_response_body)
 
@@ -74,7 +74,7 @@ def test_3_validate_object_updates_patch():
 # Validate PATCH request for updating a location record using /location endpoint
 def test_4_validate_object_updates_put():
     url = "http://127.0.0.1:8000/locations"
-    new_user = {
+    new_location = {
         "id": random.randint(
             10000, 99999
         ),  # Creates a location with the following attributes
@@ -82,7 +82,7 @@ def test_4_validate_object_updates_put():
         "country": "United States PUT",
     }
     post_object = requests.post(
-        url, json=new_user
+        url, json=new_location
     )  # POST operation for updating the new location
     response_body = post_object.json()  # Converts the response into JSON
     location_id = response_body[
@@ -99,9 +99,37 @@ def test_4_validate_object_updates_put():
     updated_response_body = patch_object.json()
     assert updated_response_body["id"] == location_id  # Validate id matches as expected
     assert (
-        updated_response_body["country"] == "United States AUTOMATED (PUT)"
+            updated_response_body["country"] == "United States AUTOMATED (PUT)"
     )  # Validate country was updated
     assert (
-        updated_response_body["city"] == "Denver AUTOMATED (PUT)"
+            updated_response_body["city"] == "Denver AUTOMATED (PUT)"
     )  # Validate city was successfully updated
+    print(updated_response_body)
+
+
+# Validate DELETE request for deleting a location record using /location endpoint
+def test_5_validate_location_object_delete():
+    url = "http://127.0.0.1:8000/locations"
+    new_location = {
+        "id": random.randint(
+            10000, 99999
+        ),  # Creates a location with the following attributes
+        "city": "Denver DELETE",
+        "country": "United States DELETE",
+    }
+    post_object = requests.post(
+        url, json=new_location
+    )  # POST operation for updating the new location
+    response_body = post_object.json()  # Converts the response into JSON
+    location_id = response_body[
+        "id"
+    ]  # Assigns the id to the location_id variable for later use
+    delete_object = requests.delete(
+        url + f"/{location_id}"
+    )  # DELETE operation for test location
+    updated_response_body = delete_object.json()
+    assert delete_object.status_code == 200
+    assert updated_response_body["message"] == "Location has been successfully deleted."
+    get_deleted_object = requests.get(url + f"{location_id}")
+    assert get_deleted_object.status_code == 404
     print(updated_response_body)
